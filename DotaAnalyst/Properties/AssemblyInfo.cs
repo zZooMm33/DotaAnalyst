ee839f7195c375e1d239275a1086c23b25741bfe
zZooMm33 <36926228+zZooMm33@users.noreply.github.com>
2020-08-16 23:08:28 +0300

    Небольшая работа с архитектурой

--- DotaAnalyst/Properties/AssemblyInfo.cs
+++ DotaAnalyst/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные со сборкой.
-[assembly: AssemblyTitle("DotaAnalyst")]
+[assembly: AssemblyTitle("Analytics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotaAnalyst")]
+[assembly: AssemblyProduct("Analytics")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
