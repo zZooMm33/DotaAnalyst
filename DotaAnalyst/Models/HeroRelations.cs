63b4c44bea0bdbf3ef1ef06386941bcc3384926f
zZooMm33 <36926228+zZooMm33@users.noreply.github.com>
2020-08-16 22:31:13 +0300

    Еще одна модель

---
+++ DotaAnalyst/Models/HeroRelations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibraryDota2.Models
+{
+    public class HeroRelations
+    {
+        private int Id { get; set; }
+
+        private int IdHero { get; set; }
+
+        private List<Item> MostUsedItems { get; set; }
+
+        private List<Hero> BestVersus { get; set; }
+
+        private List<Hero> WorstVersus { get; set; }
+    }
+}
