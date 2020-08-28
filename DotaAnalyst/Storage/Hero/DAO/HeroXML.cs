3189d53037870dbde8aed49dea0f099953e9f516
zZooMm33 <36926228+zZooMm33@users.noreply.github.com>
2020-08-28 19:08:30 +0300

    Получение героев было переделано на linq

--- DotaAnalyst/Storage/Hero/DAO/HeroXML.cs
+++ DotaAnalyst/Storage/Hero/DAO/HeroXML.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Xml;
+using System.Xml.Linq;
 using DotaAnalyst.Models;
 using DotaAnalyst.Utils;
 
@@ -13,18 +15,16 @@ namespace DotaAnalyst.Database
     {
         public List<Hero> GetHeroes()
         {
-            List<Hero> heroes = new List<Hero>();
-            XmlDocument doc = new XmlDocument();
-            doc.Load(Path.GetTempPath()
+            XDocument doc = XDocument.Load(Path.GetTempPath()
                 + ConfigurationManager.AppSettings[AppConfigConst.RESOURCES_PATH_NAME]
                 + CreateResources.HEROES_FILE_NAME);
 
-            foreach (XmlNode node in doc.DocumentElement)
-            {
-                int id = int.Parse(node["id"].InnerText);
-                string name = node["name"].InnerText;
-                heroes.Add(new Hero() { Id = id, Name = name});
-            }
+            var heroes = doc.Descendants("hero").Select(item =>
+                    new Hero()
+                    {
+                        Id = int.Parse(item.Element("id").Value),
+                        Name = item.Element("name").Value
+                    }).ToList();
 
             return heroes;
         }
